0af8ba4d58d97198736e6566d541f5af1c32fabe
Jannis Pohlmann <jannis@xfce.org>
2011-05-02 01:51:21 +0000

    - Add CODING_STYLE document. Refactor spring algorithm.

--- generic/pgf/libraries/graphdrawing/lua/algorithms/pgflibrarygraphdrawing-algorithms-spring.lua
+++ generic/pgf/libraries/graphdrawing/lua/algorithms/pgflibrarygraphdrawing-algorithms-spring.lua
@@ -37,72 +37,11 @@ function drawGraphAlgorithm_spring(graph, options)
   -- read options passed to the algorithm from TikZ
   local node_distance = graph:getOption('node distance') or 28.5
   local iterations = graph:getOption('layout iterations') or 500
-  local max_repulsive_force = graph:getOption('maximum repulsive force') or 6
+  local max_repulsion = graph:getOption('maximum repulsion') or 6
   local k = graph:getOption('spring constant') or 2
   local c = graph:getOption('IN SEARCH FOR A GOOD NAME') or 0.1 -- Johannes Textor used 0.01 here
-  local max_vertex_movement = graph:getOption('maximum iterative node movement') or 0.5
+  local max_node_movement = graph:getOption('maximum iterative node movement') or 0.5
   local positioning = graph:getOption('initial positioning') or 'circle'
-
-  -- function to compute the repulsive force between two nodes
-  local function computeRepulsiveForce(node1, node2)
-    -- compute the distance between the two nodes
-    local diff = node2.position:subtract(node1.position)
-    local d = diff:norm()
-
-    -- enforce a small distance if the nodes are located 
-    -- at the same position
-    if d < 0.1 then
-      diff:update(function (n, val) return 0.1 * math.random() + 0.1 end)
-      d = diff:norm()
-    end
-  
-    -- update the repulsive force between the two nodes 
-    -- unless they are too far way from each other already
-    if d < max_repulsive_force then
-      -- compute the repulsive force
-      local force = k * k / d
-
-      -- update the forces to be applied to the two nodes
-      node2.force:update(function (n, val) return val + force * diff:get(n) / d end)
-      node1.force:update(function (n, val) return val - force * diff:get(n) / d end)
-    end
-  end
-
-  -- function to compute the attractive forces of nodes adjacent 
-  -- to a given edge
-  local function computeAttractiveForce(edge)
-    -- determine the two nodes of the edge
-    local nodes = edge:getNodes()
-    local node1 = nodes[1]
-    local node2 = nodes[2]
-
-    -- compute the distance between the two nodes
-    local diff = node2.position:subtract(node1.position)
-    local d = diff:norm()
-
-    -- enforce a small distance if the nodes are located
-    -- at the same position
-    if d < 0.1 then
-      diff:update(function (n, val) return 0.1 * math.random() + 0.1 end)
-      d = diff:norm()
-    end
-
-    -- limit the distance to the maximum repulsive force
-    if d > max_repulsive_force then
-      d = max_repulsive_force
-    end
-
-    -- compute the attractive force between the nodes
-    local force = (d * d - k * k) / k
-
-    -- update the attractive force between the nodes, taking
-    -- the special attraction of the edge into account
-    force = force * math.log(edge.attraction) * 0.5 + 1
-
-    -- update the forces to be applied to both nodes
-    node2.force:update(function (n, val) return val - force * diff:get(n) / d end)
-    node1.force:update(function (n, val) return val + force * diff:get(n) / d end)
-  end
 
   -- get nodes of the graph in an array
   local nodes = table.asTable(graph.nodes)
@@ -128,13 +67,13 @@ function drawGraphAlgorithm_spring(graph, options)
     -- update repulsive forces between nodes in the graph
     for u = 1, #nodes do
       for v = u + 1, #nodes do
-        computeRepulsiveForce(nodes[u], nodes[v])
+        computeRepulsiveForce(nodes[u], nodes[v], k, max_repulsion)
       end
     end
 
     -- update attractive forces between adjacent nodes
     for edge in keys(graph.edges) do
-      computeAttractiveForce(edge)
+      computeAttractiveForce(edge, k, max_repulsion)
     end
 
     -- update node positions based on the new force
@@ -142,8 +81,8 @@ function drawGraphAlgorithm_spring(graph, options)
       -- create a move vector based on the force
       local move = Vector:new(2, function (n) return c * node.force:get(n) end)
 
-      -- limit the move vector to the maximum allowed vertex movement
-      move:limit(function (k, v) return -max_vertex_movement, max_vertex_movement end)
+      -- limit the move vector to the maximum allowed node movement
+      move:limit(function (k, v) return -max_node_movement, max_node_movement end)
 
       -- move the node
       node.position:update(function (n, val) return val + move:get(n) end)
@@ -155,7 +94,6 @@ function drawGraphAlgorithm_spring(graph, options)
 
   -- position nodes according to the desired node distance
   for node in values(nodes) do
-    Sys:logMessage('spring: ' .. node.name .. ' at ' .. tostring(node.position))
     node.pos.x =  node.position:get(1) * node_distance
     node.pos.y = -node.position:get(2) * node_distance
   end
@@ -196,4 +134,70 @@ function getPositioningFunction(graph, nodes, technique)
   elseif technique == 'origin' or true then
     return function (n) return 0 end
   end
+end
+
+
+
+--- Function to compute the repulsive force between two nodes.
+--
+function computeRepulsiveForce(node1, node2, k, max_repulsion)
+  -- compute the distance between the two nodes
+  local diff = node2.position:subtract(node1.position)
+  local d = diff:norm()
+
+  -- enforce a small distance if the nodes are located 
+  -- at the same position
+  if d < 0.1 then
+    diff:update(function (n, val) return 0.1 * math.random() + 0.1 end)
+    d = diff:norm()
+  end
+
+  -- update the repulsive force between the two nodes 
+  -- unless they are too far way from each other already
+  if d < max_repulsion then
+    -- compute the repulsive force
+    local force = k * k / d
+
+    -- update the forces to be applied to the two nodes
+    node2.force:update(function (n, val) return val + force * diff:get(n) / d end)
+    node1.force:update(function (n, val) return val - force * diff:get(n) / d end)
+  end
+end
+
+
+
+--- Function to compute the attractive forces of nodes adjacent to a given edge.
+--
+function computeAttractiveForce(edge, k, max_repulsion)
+  -- determine the two nodes of the edge
+  local nodes = edge:getNodes()
+  local node1 = nodes[1]
+  local node2 = nodes[2]
+
+  -- compute the distance between the two nodes
+  local diff = node2.position:subtract(node1.position)
+  local d = diff:norm()
+
+  -- enforce a small distance if the nodes are located
+  -- at the same position
+  if d < 0.1 then
+    diff:update(function (n, val) return 0.1 * math.random() + 0.1 end)
+    d = diff:norm()
+  end
+
+  -- limit the distance to the maximum repulsive force
+  if d > max_repulsion then
+    d = max_repulsion
+  end
+
+  -- compute the attractive force between the nodes
+  local force = (d * d - k * k) / k
+
+  -- update the attractive force between the nodes, taking
+  -- the special attraction of the edge into account
+  force = force * math.log(edge.attraction) * 0.5 + 1
+
+  -- update the forces to be applied to both nodes
+  node2.force:update(function (n, val) return val - force * diff:get(n) / d end)
+  node1.force:update(function (n, val) return val + force * diff:get(n) / d end)
 end
