f05f881ca30eaeafd0c67ba841705200460fb583
Christophe Jorssen <cjorssen@users.sourceforge.net>
2011-06-02 21:32:07 +0000

    Some work on a lua (lpeg) math parser

--- generic/pgf/libraries/luamath/pgfluamath.parser.lua
+++ generic/pgf/libraries/luamath/pgfluamath.parser.lua
@@ -7,9 +7,135 @@
 --
 -- See the file doc/generic/pgf/licenses/LICENSE for more details.
 --
---	$Id$	
+-- $Id$	
 --
+-- See http://journal.stuffwithstuff.com/2011/03/19/pratt-parsers-expression-parsing-made-easy/ for precedence with peg grammar
+
 module("pgfluamath.parser", package.seeall)
+
+-- From pgfmathparser.code.tex
+local operator_table = {
+   [1] = {
+      symbol = "+",
+      name = "add",
+      arity = 2,
+      fixity = "infix",
+      precedence = 500},
+   [2] = {
+      symbol = "-",
+      name = "substract",
+      arity = 2,
+      fixity = "infix",
+      precedence = 500},
+   [3] = {
+      symbol = "*",
+      name = "multiply",
+      arity = 2,
+      fixity = "infix",
+      precedence = 700},
+   [4] = {
+      symbol = "/",
+      name = "divide",
+      arity = 2,
+      fixity = "infix",
+      precedence = 700},
+   [5] = {
+      symbol = "^",
+      name = "pow",
+      arity = 2,
+      fixity = "infix",
+      precedence = 900},
+   [6] = {
+      symbol = ">",
+      name = "greater",
+      arity = 2,
+      fixity = "infix",
+      precedence = 250},
+   [7] = {
+      symbol = "<",
+      name = "less",
+      arity = 2,
+      fixity = "infix",
+      precedence = 250},
+   [8] = {
+      symbol = "==",
+      name = "equal",
+      arity = 2,
+      fixity = "infix",
+      precedence = 250},
+   [9] = {
+      symbol = ">=",
+      name = "notless",
+      arity = 2,
+      fixity = "infix",
+      precedence = 250},
+   [10] = {
+      symbol = "<=",
+      name = "notgreater",
+      arity = 2,
+      fixity = "infix",
+      precedence = 250},
+   [11] = {
+      symbol = "&&",
+      name = "and",
+      arity = 2,
+      fixity = "infix",
+      precedence = 200},
+   [12] = {
+      symbol = "||",
+      name = "or",
+      arity = 2,
+      fixity = "infix",
+      precedence = 200},
+   [13] = {
+      symbol = "!=",
+      name = "notequalto",
+      arity = 2,
+      fixity = "infix",
+      precedence = 250},
+   [14] = {
+      symbol = "!",
+      name = "not",
+      arity = 1,
+      fixity = "prefix",
+      precedence = 975},
+   [15] = {
+      symbol = "?",
+      name = "ifthenelse",
+      arity = 3,
+      fixity = "infix",
+      precedence = 100},
+   [16] = {
+      symbol = ":",
+      name = "@@collect",
+      arity = 2,
+      fixity = "infix",
+      precedence = 101},
+   [17] = {
+      symbol = "!",
+      name = "factorial",
+      arity = 1,
+      fixity = "postfix",
+      precedence = 800},
+   [18] = {
+      symbol = "r",
+      name = "deg",
+      arity = 1,
+      fixity = "postfix",
+      precedence = 600},
+}
+
+--[[ Dont't think those are needed?
+\pgfmathdeclareoperator{,}{@collect}   {2}{infix}  {10}
+\pgfmathdeclareoperator{[}{@startindex}{2}{prefix} {7}
+\pgfmathdeclareoperator{]}{@endindex}  {0}{postfix}{6}
+\pgfmathdeclareoperator{(}{@startgroup}{1}{prefix} {5}
+\pgfmathdeclareoperator{)}{@endgroup}  {0}{postfix}{4}
+\pgfmathdeclareoperator{\pgfmath@bgroup}{@startarray}{1}{prefix} {3}
+\pgfmathdeclareoperator{\pgfmath@egroup}{@endarray}  {1}{postfix}{2}% 
+\pgfmathdeclareoperator{"}{}{1}{prefix}{1} --"
+\pgfmathdeclareoperator{@}{}{0}{postfix}{0}
+--]]
 
 local S, P, R = lpeg.S, lpeg.P, lpeg.R
 local C, Cc, Ct = lpeg.C, lpeg.Cc, lpeg.Ct
@@ -17,14 +143,11 @@ local V = lpeg.V
 
 local space_pattern = S(" \n\r\t")^0
 
--- All numbers are positive, ie -5 = neg(5). If not, uncomment...
--- local integer_pattern = P("-")^-1 * R("09")^1
-local integer_pattern = R("09")^1
+local integer_pattern = P("-")^-1 * R("09")^1
 local positive_decimal_pattern = (R("09")^1 * P(".") * R("09")^1) +
                                  (P(".") * R("09")^1) +
 			         (R("09")^1 * P("."))
--- local decimal_pattern = P("-")^-1 * positive_decimal_pattern
-local decimal_pattern = positive_decimal_pattern
+local decimal_pattern = P("-")^-1 * positive_decimal_pattern
 local integer = Cc("integer") * C(integer_pattern) * space_pattern
 local decimal = Cc("decimal") * C(decimal_pattern) * space_pattern
 local float = Cc("float") * C(decimal_pattern) * S("eE") * 
@@ -33,8 +156,8 @@ local float = Cc("float") * C(decimal_pattern) * S("eE") *
 local openparen = Cc("openparen")  * C(P("(")) * space_pattern
 local closeparen = Cc("closeparen") * C(P(")")) * space_pattern
 
-local addop = Cc("addop") * C(S("+")) * space_pattern
-local mulop = Cc("mulop") * C(S("*")) * space_pattern
+local addop = Cc("addop") * C(S("+-")) * space_pattern
+local mulop = Cc("mulop") * C(S("*/")) * space_pattern
 
 local grammar = P {
    "E",
