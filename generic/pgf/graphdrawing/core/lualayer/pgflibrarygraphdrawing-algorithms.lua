7dcdb539723bbde95aee3ae0faff48602878c9a6
Jannis Pohlmann <jannis@xfce.org>
2011-09-19 22:48:00 +0000

    graph drawing: Fix the incredibly broken Floyd-Warshall implementation.

--- generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-algorithms.lua
+++ generic/pgf/graphdrawing/core/lualayer/pgflibrarygraphdrawing-algorithms.lua
@@ -87,26 +87,26 @@ end
 
 function algorithms.floyd_warshall(graph)
   local distance = {}
-  local infinity = #graph.nodes + 1
+  local infinity = math.huge
+
+  for i in table.value_iter(graph.nodes) do
+    distance[i] = {}
+    for j in table.value_iter(graph.nodes) do
+      distance[i][j] = infinity
+    end
+  end
 
   for i in table.value_iter(graph.nodes) do
     for edge in table.value_iter(i.edges) do
       local j = edge:getNeighbour(i)
-
-      distance[i] = distance[i] or {}
       distance[i][j] = edge.weight or 1
     end
   end
 
-  for k = 1, #graph.nodes do
-    for i = 1, #graph.nodes do
-      for j = i + 1, #graph.nodes do
-        local d_ij = (distance[i] and distance[i][j]) and distance[i][j] or infinity
-        local d_ik = (distance[i] and distance[i][k]) and distance[i][k] or infinity
-        local d_kj = (distance[k] and distance[k][j]) and distance[k][j] or infinity
-
-        distance[i] = distance[i] or {}
-        distance[i][j] = math.min(d_ij, d_ik + d_kj)
+  for k in table.value_iter(graph.nodes) do
+    for i in table.value_iter(graph.nodes) do
+      for j in table.value_iter(graph.nodes) do
+        distance[i][j] = math.min(distance[i][j], distance[i][k] + distance[k][j])
       end
     end
   end
