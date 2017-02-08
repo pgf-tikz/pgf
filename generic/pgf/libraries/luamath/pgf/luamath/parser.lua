cc4094a7ab981eb5567006b27c55a9eccd48468a
Christian Feuersaenger <cfeuersaenger@users.sourceforge.net>
2017-02-08 19:52:09 +0000

    Fixed luamath library and 'declare function'
    if the declared function dependend on a macro, the value was either evaluated in a wrong way or lead to a compile error

--- generic/pgf/libraries/luamath/pgf/luamath/parser.lua
+++ generic/pgf/libraries/luamath/pgf/luamath/parser.lua
@@ -142,6 +142,9 @@ local prefix_operator_pattern = (prefix_operator * space_pattern * Cg(Prefix) ) 
 -- apparently, we need to distinghuish between <expr> ! and  <expr> != <expr2>:
 local postfix_operator = C( S"r!" - P"!=" )  + C(P"^") * space_pattern * pow_exponent
 
+pgfluamathfunctions.functionMustBeEvaluatedInTeX = function()
+	error("The function in this context cannot be evaluated by LUA because it depends on TeX macros.")
+end
 
 local ternary_eval = pgfluamathfunctions.ifthenelse
 
