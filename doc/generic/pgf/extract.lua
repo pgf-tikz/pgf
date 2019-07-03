78933691f834361aa3ae3a71b8c60ff161b1b22c
Henri Menke <henri@icp.uni-stuttgart.de>
2019-07-04 10:22:32 +1200

    extract.lua: recurse into subdirectories, ignore remember picture

--- doc/generic/pgf/extract.lua
+++ doc/generic/pgf/extract.lua
@@ -88,75 +88,104 @@ local extractor = lpeg.P{"document",
 }
 
 -- get the basename and extension of a file
-local basename = function(file)
+local function basename(file)
     local basename, ext = string.match(file, "^(.+)%.([^.]+)$")
     return basename or "",  ext or file
 end
 
--- Main loop
-if #arg ~= 2 then
-    print("Usage: " .. arg[-1] .. " " .. arg[0] .. " <source-dir> <target-dir>")
-    os.exit(1)
-end
 local pathsep = package.config:sub(1,1)
-sourcedir = arg[1] .. pathsep
-targetdir = arg[2] .. pathsep
-assert(lfs.attributes(sourcedir, "mode") == "directory", sourcedir .. " is not a directory")
-assert(lfs.attributes(targetdir, "mode") == "directory", targetdir .. " is not a directory")
 
-for file in lfs.dir(sourcedir) do
-    if lfs.attributes(sourcedir .. file, "mode") == "file" then
-        print("Processing " .. file)
+-- Walk the file tree
+local function walk(sourcedir, targetdir)
+    -- Make sure the arguments are directories
+    assert(lfs.attributes(sourcedir, "mode") == "directory", sourcedir .. " is not a directory")
+    assert(lfs.attributes(targetdir, "mode") == "directory", targetdir .. " is not a directory")
 
-        -- Read file into memory
-        local f = io.open(sourcedir .. file)
-        local text = f:read("*all")
-        f:close()
-        local name, ext = basename(file)
+    -- Append the path separator if necessary
+    if sourcedir:sub(-1, -1) ~= pathsep then
+        sourcedir = sourcedir .. pathsep
+    end
+    if targetdir:sub(-1, -1) ~= pathsep then
+        targetdir = targetdir .. pathsep
+    end
 
-        -- preprocess, strip all commented lines
-        text = text:gsub("\n%%[^\n]*","")
+    -- Process all items in the directory
+    for file in lfs.dir(sourcedir) do
+        if file == "." or file == ".." then
+            -- Ignore these two special ones
+        elseif lfs.attributes(sourcedir .. file, "mode") == "directory" then
+            -- Recurse into subdirectories
+            lfs.mkdir(targetdir .. file)
+            walk(sourcedir .. file .. pathsep, targetdir .. file .. pathsep)
+        elseif lfs.attributes(sourcedir .. file, "mode") == "file" then
+            print("Processing " .. sourcedir .. file)
 
-        -- extract all code examples
-        local matches = extractor:match(text) or {}
+            -- Read file into memory
+            local f = io.open(sourcedir .. file)
+            local text = f:read("*all")
+            f:close()
+            local name, ext = basename(file)
 
-        -- write code examples to separate files
-        local setup_code = ""
-        for n, e in ipairs(matches) do
-            local options = e[1]
-            local content = e[2]
+            -- preprocess, strip all commented lines
+            text = text:gsub("\n%%[^\n]*","")
 
-            -- If the snippet is marked as setup code, we have to put it before
-            -- every other snippet in the same file
-            if options["setup code"] then
-                setup_code = setup_code .. strip(content) .. "\n"
-            end
+            -- extract all code examples
+            local matches = extractor:match(text) or {}
 
-            -- Skip those that say "code only"
-            if not options["code only"] then
-                local newname = name .. "-" .. n .. "." .. ext
-                local examplefile = io.open(targetdir .. newname, "w")
+            -- write code examples to separate files
+            local setup_code = ""
+            for n, e in ipairs(matches) do
+                local options = e[1]
+                local content = e[2]
 
-                examplefile:write"\\documentclass{article}\n"
-                examplefile:write"\\usepackage{fp,pgf,tikz,xcolor}\n"
-                examplefile:write(preamble) -- TODO: this has to go
-                examplefile:write(options["preamble"] and options["preamble"] .. "\n" or "")
-                examplefile:write"\\begin{document}\n"
-                examplefile:write"\\makeatletter\n" -- TODO: this has to go
-                examplefile:write(setup_code)
-                local pre = options["pre"] or ""
-                pre = pre:gsub("##", "#")
-                examplefile:write(pre .. "\n")
-                if options["render instead"] then
-                    examplefile:write(options["render instead"] .. "\n")
-                else
-                    examplefile:write(strip(content) .. "\n")
+                if content:match("remember picture") then
+                    goto continue
                 end
-                examplefile:write(options["post"] and options["post"] .. "\n" or "")
-                examplefile:write"\\end{document}\n"
 
-                examplefile:close()
+                -- If the snippet is marked as setup code, we have to put it before
+                -- every other snippet in the same file
+                if options["setup code"] then
+                    setup_code = setup_code .. strip(content) .. "\n"
+                    goto continue
+                end
+
+                -- Skip those that say "code only"
+                if not options["code only"] then
+                    local newname = name .. "-" .. n .. "." .. ext
+                    local examplefile = io.open(targetdir .. newname, "w")
+
+                    examplefile:write"\\documentclass{article}\n"
+                    examplefile:write"\\usepackage{fp,pgf,tikz,xcolor}\n"
+                    examplefile:write(preamble) -- TODO: this has to go
+                    examplefile:write(options["preamble"] and options["preamble"] .. "\n" or "")
+                    examplefile:write"\\begin{document}\n"
+                    examplefile:write"\\makeatletter\n" -- TODO: this has to go
+                    examplefile:write(setup_code)
+                    local pre = options["pre"] or ""
+                    pre = pre:gsub("##", "#")
+                    examplefile:write(pre .. "\n")
+                    if options["render instead"] then
+                        examplefile:write(options["render instead"] .. "\n")
+                    else
+                        examplefile:write(strip(content) .. "\n")
+                    end
+                    examplefile:write(options["post"] and options["post"] .. "\n" or "")
+                    examplefile:write"\\end{document}\n"
+
+                    examplefile:close()
+                end
+
+                ::continue::
             end
         end
     end
+end
+
+-- Main loop
+if #arg < 2 then
+    print("Usage: " .. arg[-1] .. " " .. arg[0] .. " <source-dirs...> <target-dir>")
+    os.exit(1)
+end
+for n = 1, #arg - 1 do
+    walk(arg[n], arg[#arg])
 end
