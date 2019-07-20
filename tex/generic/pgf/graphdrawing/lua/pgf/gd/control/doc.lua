26e55a3829c3110930c040be9caef66aeaa1150d
Stefan Pinnow <Mo-Gul@gmx.net>
2019-07-20 23:53:45 +0200

    - corrected wrongly commented Lua comments in the Lua documentation files (of commit 900d47729d91dd9ba3eb59de56d5d9a4ba2eb155

--- tex/generic/pgf/graphdrawing/lua/pgf/gd/control/doc.lua
+++ tex/generic/pgf/graphdrawing/lua/pgf/gd/control/doc.lua
@@ -98,11 +98,11 @@ sense that some nodes are ``nailed to the canvas'' while other
 nodes can ``move freely''.
 ]]
 
-[[
+--[[
 % TODOsp: codeexamples: the following 3 examples need these libraries
 %    \usetikzlibrary{graphs,graphdrawing}
 %    \usegdlibrary{force}
-]]
+--]]
 example
 [[
 \begin{tikzpicture}
@@ -167,11 +167,11 @@ Note how in the last example |c| is placed at |(1,1)| rather than
 |b| as would happen by default.
 ]]
 
-[[
+--[[
 % TODOsp: codeexamples: the following 4 examples need these libraries
 %    \usetikzlibrary{graphs,graphdrawing}
 %    \usegdlibrary{layered}
-]]
+--]]
 example
 [[
 \tikz \draw (0,0)
