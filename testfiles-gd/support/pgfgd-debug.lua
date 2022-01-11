6187845bf3fcbf27156ae7436eb3532161e0c77f
Henri Menke <henri@henrimenke.de>
2022-01-11 09:15:05 +0100

    test(gd): optimize Lua code
    Signed-off-by: Henri Menke <henri@henrimenke.de>

--- testfiles-gd/support/pgfgd-debug.lua
+++ testfiles-gd/support/pgfgd-debug.lua
@@ -1,39 +1,32 @@
-local InterfaceToDisplay = pgf.gd.interface.InterfaceToDisplay
-
---- Wrap InterfaceToDisplay functions to prepend debugging code
-local createVertex = InterfaceToDisplay.createVertex
--- here `...` is a vararg expression, 
--- see https://www.lua.org/manual/5.3/manual.html#3.4.11
-function InterfaceToDisplay.createVertex(...)
-  local name = ...
-  debug("Create vertex '%s'", name)
-  createVertex(...)
-end
-
-local createEdge = InterfaceToDisplay.createEdge
-function InterfaceToDisplay.createEdge(...)
-  local tail, head, direction = ...
-  debug("Create edge '%s' from '%s' to '%s'", direction, tail, head)
-  createEdge(...)
-end
-
--- this generates too many debugging lines
--- local createEvent = InterfaceToDisplay.createEvent
--- function InterfaceToDisplay.createEvent(...)
---   local kind = ...
---   debug("Create event '%s'", kind)
---   return createEvent(...)
--- end
-
-local addToVertexOptions = InterfaceToDisplay.addToVertexOptions
-function InterfaceToDisplay.addToVertexOptions(...)
-  local name = ...
-  debug("Add options to vertex '%s'", name)
-  addToVertexOptions(...)
-end
-
+local InterfaceToDisplay = assert(pgf.gd.interface.InterfaceToDisplay)
 
 -- helper
-function debug(format_str, ...)
-  tex.sprint(string.format("\\pgfgdluainfo{" .. format_str .. "}", ...))
+local function typeout(...)
+  texio.write_nl(17, string.format(...))
+end
+
+local createVertex = assert(InterfaceToDisplay.createVertex)
+function InterfaceToDisplay.createVertex(name, ...)
+  typeout("Create vertex '%s'", name)
+  createVertex(name, ...)
+end
+
+local createEdge = assert(InterfaceToDisplay.createEdge)
+function InterfaceToDisplay.createEdge(tail, head, direction, ...)
+  typeout("Create edge '%s' from '%s' to '%s'", direction, tail, head)
+  createEdge(tail, head, direction, ...)
+end
+
+--[[ this generates too many debugging lines
+local createEvent = assert(InterfaceToDisplay.createEvent)
+function InterfaceToDisplay.createEvent(kind, ...)
+  typeout("Create event '%s'", kind)
+  return createEvent(kind, ...)
+end
+--]]
+
+local addToVertexOptions = assert(InterfaceToDisplay.addToVertexOptions)
+function InterfaceToDisplay.addToVertexOptions(name, ...)
+  typeout("Add options to vertex '%s'", name)
+  addToVertexOptions(name, ...)
 end
