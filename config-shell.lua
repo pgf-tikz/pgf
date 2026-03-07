b2e1c5f501408c93d4ac85cd7434fdc65aa19fcf
Karl Hammond <kaiserkarl31@yahoo.com>
2026-03-07 14:25:44 -0600

    Revert "Merge branch 'pgf-tikz:master' into typo-fixes"
    This reverts commit bbfca8851f9d3df964d13183c1cca13f23748118, reversing
    changes made to 7d19be9f1c501c8809f7643a83c813e47ac70ff5.

--- config-shell.lua
+++
@@ -1,7 +0,0 @@
--- Tests which require shell escape enabled
-
-testfiledir  = "testfiles-shell"
-checkopts    = "-interaction=nonstopmode -shell-escape"
-
--- files removed between different engine tests
-dynamicfiles = {"*.gnuplot", "*.table"}
